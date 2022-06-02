ecb553faa2ba6d1d198507142a93a2564b76234c
witaradya <witaradya.a.dharma@polytron.co.id>
2022-06-02 13:51:40 +0700

    Finish day 1 challange

---
+++ day1_BandNameGenerator.py
@@ -0,0 +1,31 @@
+# Day 1 Project : Band Name Generator
+# print("Day 1 - Python Print Function")
+# print("This function is declared like this : ")
+# print("print('what to print')")
+
+# print("Hello " + input("Who is your name ") + " !")
+
+# a = len(input("My name is "))
+# print(a)
+
+# a = input('a : ')
+# b = input('b : ')
+
+# c = a
+# a = b
+# b = c
+
+# print("a : " + a)
+# print("b : " + b)
+
+# print("Welcome to the Band Name Generator")
+# print("What's name of the city you grew up in?")
+# city = input()
+# print("What's your pet's name?")
+# pet = input()
+# print("Your band name could be " + city + " " + pet)
+
+print("Welcome to the Band Name Generator")
+city = input("What's name of city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
